85dc7581866e7bbf38e439b2cff9a74972d98cd1
Raissa Peneluc <raissapeneluc@gmail.com>
2024-07-22 16:58:03 -0300

    Documentando o código.

--- src/Journey.Api/Filters/ExceptionFilter.cs
+++ src/Journey.Api/Filters/ExceptionFilter.cs
@@ -16,18 +16,24 @@ namespace Journey.Api.Filters
         // OneExeception() -> Vai ser chamado automaticamente quando uma exceção não tratada ocorre durante o processamento de uma requisição. Ele modifica a resposta HTTP com base no tipo da exceção capturada.
         public void OnException(ExceptionContext context)
         {
+            // Verifica se a exceção capturada é do tipo JourneyException.
             if (context.Exception is JourneyException)
             {
+                // Faz o cast da exceção para JourneyException.
                 var journeyException = (JourneyException)context.Exception;
 
+                // Define o status code da resposta HTTP baseado no status code retornado pela exceção.
                 context.HttpContext.Response.StatusCode = (int)journeyException.GetStatusCode();
 
+                // Cria um objeto de resposta com as mensagens de erro da exceção.
                 var responseJson = new ResponseErrorsJson(journeyException.GetErrorsMessages());
 
+                // Define o resultado da ação como um ObjectResult contendo o objeto de resposta.
                 context.Result = new ObjectResult(responseJson);
             }
             else 
             {
+
                 context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
 
                 var list = new List<string>

b90f075d4d54ee9c24921c109ef96967ae5bc0d8
Raissa Peneluc <raissapeneluc@gmail.com>
2024-08-02 16:55:30 -0300

    Criando Regras de Negócios para as Activities.

--- src/Journey.Application/UseCases/Activities/Register/RegisterActivityForTripUseCase.cs
+++ src/Journey.Application/UseCases/Activities/Register/RegisterActivityForTripUseCase.cs
@@ -1,12 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation.Results;
+using Journey.Communication.Requests;
+using Journey.Communication.Responses;
+using Journey.Exception;
+using Journey.Exception.ExceptionsBase;
+using Journey.Infrastructure;
+using Journey.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+/* Caso de Uso onde essa classe é responsável
+ * por ser uma regra de negócio, onde a request
+ * vai ser validada e também persistir no banco
+ * de dados. */
 
 namespace Journey.Application.UseCases.Activities.Register
 {
-    internal class RegisterActivityForTripUseCase
+    public class RegisterActivityForTripUseCase
     {
+        // ResponseActivityJson -> O Execute vai devolver essa classe.
+        public ResponseActivityJson Execute(Guid tripId, RequestRegisterActivityJson request)
+        {
+            // Realizando a instância de um dbContext.
+            var dbContext = new JourneyDbContext();
+
+            // Indo na tabela de viagens pegando as atividades e o id.
+            var trip = dbContext
+                .Trips
+                .Include(trip => trip.Activities)
+                .FirstOrDefault(trip => trip.Id == tripId);
+
+            if (trip is null)
+            {
+                throw new NotFoundException(ResourceErrorMessages.TRIP_NOT_FOUND);
+            }
+
+
+            return null;
+        }
+
+        // Validação da request.
+        private void Validate(Trip trip, RequestRegisterActivityJson request) 
+        {
+            // Criando a instância do Validator.
+            var validator = new RegisterActivityValidator();
+
+            // Utilizando o Validator para validar a request.
+            var result = validator.Validate(request);
+
+            // Verificando se a data da request está sendo compreendida pelo início e fim da trip. 
+            if((request.Date >= trip.StartDate && request.Date <= trip.EndDate) == false)
+            {
+                result.Errors.Add(new ValidationFailure();
+            }
+
+            // Verificando se o resultado ele é falso.
+            if (result.IsValid == false)
+            {
+                // Se ele for falso, significa que existem erros de validação, então ele vai nos erros, buscando especificamente as mensagem de erro para recuperá-las.
+                // Recupera as mensagens de erro do Validate.
+                var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+
+                throw new ErrorOnValidationException(errorMessages);
+            }
+        }
+        
     }
 }
---
+++ src/Journey.Application/UseCases/Activities/Register/RegisterActivityValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Journey.Communication.Requests;
+using Journey.Exception;
+
+/* Esta classe usa o pacote FluentValidation para validar as
+ * propriedades de um objeto do tipo RequestRegisterTripJson. 
+ * O objetivo é garantir que os dados fornecidos em uma requisição
+ * para registrar uma atividade (Activity) atendam a determinados critérios.  */
+
+namespace Journey.Application.UseCases.Activities.Register
+{
+    // A responsabilidade do Validator é somente validar os dados da requisição (RequestRegisterActivityJson).
+    public class RegisterActivityValidator : AbstractValidator<RequestRegisterActivityJson>
+    {
+        // Onde vão ser feitas as regras para o register.
+        public RegisterActivityValidator()
+        {
+            RuleFor(request => request.Name).NotEmpty().WithMessage(ResourceErrorMessages.NAME_EMPTY);
+        }
+    }
+}
